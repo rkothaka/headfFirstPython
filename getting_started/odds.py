e19f58818bc5479e623ed9fa80e0cf334bbd9cc4
Rohan Kothakapu <rkothakapu@Rohans-MBP.lan>
2023-07-25 07:53:09 -0400

    chapter 1 - completed

--- getting_started/odds.py
+++ getting_started/odds.py
@@ -1,9 +1,15 @@
 from datetime import datetime
+import time
+import random
 
 odds = list(range(1, 60, 2))
-right_this_minute = datetime.today().minute
 
-if right_this_minute in odds:
-    print("This minute seems a little odd -", right_this_minute)
-else:
-    print("Not an odd minute -", right_this_minute)
+for i in range(5):
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds:
+        print("This minute seems a little odd -", right_this_minute)
+    else:
+        print("Not an odd minute -", right_this_minute)
+
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
